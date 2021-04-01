78cdddeb2b18888e23402d1408f81cced472a907
maxim <mksmkopyl@gmail.com>
2021-04-01 14:20:38 +0300

    add page input

--- src/pages/Title/index.js
+++ src/pages/Title/index.js
@@ -1,20 +1,42 @@
-import React, {useEffect} from "react";
+import React, {useState} from "react";
+import firebase from "../../Base";
+import {Container, Navbar} from "react-bootstrap";
 
-const TitleInput = (spells) => {
-  if (spells.length<0) {
-    return (
-      <div>Loading..</div>
-    )
-  } else {
-    return (
-      <div>
-        {spells.map(item => {
-          return item.title
-        })}
-      </div>
-    )
-  }
-
+const TitleInput = (id) => {
+  const [loading, setLoading] =React.useState(true);
+  const [spell, setSpell] = React.useState({});
+  const [currentUser, setCurrentUser] = useState()
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const db = firebase.firestore();
+      const data = await db.collection("pages").get();
+      setSpell({...data.docs.find(doc => doc.id==id.id).data()})
+    } catch (e) {} finally {setLoading(false);}
+  };
+  React.useEffect(() => {
+    setCurrentUser(localStorage.getItem('email'));
+    fetchData();
+  }, [])
+  if(loading) return null
+  return(
+    <body>
+    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+      <Navbar.Brand href="/"><h2>Profile</h2></Navbar.Brand>
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+      </Container>
+      <Navbar.Brand>
+        {currentUser && <h5>Admin {currentUser}</h5>}
+      </Navbar.Brand>
+    </Navbar>
+    <div>
+     <h2>{spell.title}</h2>
+      <h2>{spell.desc}</h2>
+        <h2>{new Date(spell.createdAt.toDate()).toDateString()}</h2>
+    </div>
+    </body>
+  )
 };
 
 export default TitleInput;
